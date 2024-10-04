1b515a88f1bb722ea95fdb22e3edd3af84bc5bac
Shah, Chirag <shahch@ornl.gov>
2024-10-04 15:06:25 -0400

    use hostname to determine env

--- src/config.js
+++ src/config.js
@@ -2,14 +2,15 @@ import DevConfig from "./config/config_dev.js"
 import StageConfig from "./config/config_stage.js"
 import ProdConfig from "./config/config_prod.js"
 
+const host  = window.location.hostname
 let Config = DevConfig
 
-if (process.env.NODE_ENV == "production") {
-  Config = ProdConfig 
-} else if (process.env.NODE_ENV == "stage") {
+if (host.includes("localhost") || host.includes("dev")) {
+  Config = DevConfig
+} else if (host.includes("stage")) {
   Config = StageConfig 
 } else {
-  Config = DevConfig
+  Config = ProdConfig 
 }
 
 export default Config
